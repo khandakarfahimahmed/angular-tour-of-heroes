41a7bae3213ef14a38ff3b6f5559c93776908f25
khandakarfahimahmed <fahim363501@gmail.com>
2024-02-13 20:37:49 +0600

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+@NgModule({
+  declarations: [AppComponent, HeroDetailComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HeroesComponent],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
